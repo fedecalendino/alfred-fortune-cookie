5fffae667a57194b58a5227ec855aa26c45acc3e
Fede Calendino <fede@calendino.com>
2022-12-18 09:36:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## ALFRED-FORTUNE-COOKIE
+
+[Alfred Workflow](https://www.alfredapp.com/workflows/) to read your fortune! 🥠️
+
---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+rm -rf "./dist/"
+
+echo "Copy dependencies"
+cp -r $(find .venv | egrep "site-packages$") ./dist
+
+echo "Clean up dist folder"
+cd dist
+ls -d */ | grep info | xargs rm -rf
+ls | egrep "^\_.*" | xargs rm -rf
+
+rm -rf *.so black blackd blib2to3 distutils* pkg_resources pip* setuptools* wheel*
+
+cd ..
+
+echo "Copy source code"
+cp -r ./src/* dist
+
+echo "Finished"
+echo
